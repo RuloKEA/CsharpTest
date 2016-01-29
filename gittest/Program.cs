bb56bb2d6a5dc25e826e108ca641204b952428d0
Rune Lyng <rulo@kea.dk>
2016-01-29 13:11:43 +0100

    Added user and cl

---
+++ gittest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+    }
+}
